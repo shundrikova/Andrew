59fc2ff35bacd83bd7ac1975b7a2f94901753b28
Sasha Shundrikova <tim.bread@yandex.ru>
2017-10-12 15:43:08 +0300

    added interface

---
+++ Andrew/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Andrew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.construct = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Lavender;
+            this.panel1.Controls.Add(this.clear);
+            this.panel1.Controls.Add(this.construct);
+            this.panel1.Location = new System.Drawing.Point(0, 493);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(584, 495);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
+            // 
+            // construct
+            // 
+            this.construct.BackColor = System.Drawing.Color.White;
+            this.construct.Location = new System.Drawing.Point(124, 14);
+            this.construct.Name = "construct";
+            this.construct.Size = new System.Drawing.Size(155, 37);
+            this.construct.TabIndex = 0;
+            this.construct.Text = "Construct convex hull";
+            this.construct.UseVisualStyleBackColor = false;
+            this.construct.Click += new System.EventHandler(this.construct_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.White;
+            this.clear.Location = new System.Drawing.Point(285, 14);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(155, 37);
+            this.clear.TabIndex = 1;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Andrew\'s algorithm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button construct;
+    }
+}
+
---
+++ Andrew/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Andrew
+{
+    public partial class Form1 : Form
+    {
+        private Pen pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void construct_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromImage(pictureBox.Image);
+            g.Clear(Color.White);
+            g.Dispose();
+            pictureBox.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Black, 1);
+            pen.StartCap = pen.EndCap = LineCap.Round;
+
+            Image bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            g.Dispose();
+            if (pictureBox.Image != null)
+                pictureBox.Image.Dispose();
+            pictureBox.Image = bmp;
+        }
+
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics g = Graphics.FromImage(pictureBox.Image);
+            g.DrawEllipse(pen, new Rectangle(e.X, e.Y, 1, 1));
+            g.Dispose();
+            pictureBox.Invalidate();
+        }
+    }
+}
