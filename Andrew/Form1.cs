ee2f3d63743f8007e1afea3bb31ad43ea79b4b23
Sasha Shundrikova <tim.bread@yandex.ru>
2017-10-12 17:28:42 +0300

    added Andrew :feelsgood:

--- Andrew/Form1.cs
+++ Andrew/Form1.cs
@@ -14,19 +14,70 @@ namespace Andrew
     public partial class Form1 : Form
     {
         private Pen pen;
+        //byte[] original;
+        List<Point> points;
+        List<Point> convex_hull;
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        private int cross(Point o, Point a, Point b)
+        {
+            return (a.X - o.X) * (b.Y - o.Y) - (a.Y - o.Y) * (b.X - o.X);
+        }
+
+        private void Andrew()
+        {
+            if (points.Count < 3) return;
+            var sorted_points = points.OrderBy(p => p.X).ThenBy(p => p.Y);
+            List<Point> up = new List<Point>();
+            List<Point> down = new List<Point>();
+            Point p1 = sorted_points.First(), p2 = sorted_points.Last();
+            up.Add(p1);
+            down.Add(p1);
+            for (int i = 1; i < sorted_points.Count(); ++i)
+            {
+                if (i == sorted_points.Count() - 1 || cross(p1, sorted_points.ElementAt(i), p2) < 0)
+                {
+                    while (up.Count >= 2 && cross(up[up.Count - 2], up[up.Count - 1], sorted_points.ElementAt(i)) >= 0)
+                        up.RemoveAt(up.Count - 1);
+                    up.Add(sorted_points.ElementAt(i));
+                }
+                if (i == sorted_points.Count() - 1 || cross(p1, sorted_points.ElementAt(i), p2) > 0)
+                {
+                    while (down.Count >= 2 && cross(down[down.Count - 2], down[down.Count - 1], sorted_points.ElementAt(i)) <= 0)
+                        down.RemoveAt(down.Count - 1);
+                    down.Add(sorted_points.ElementAt(i));
+                }
+            }
+            for (int i = 0; i < up.Count; ++i)
+                if(!convex_hull.Contains(up[i]))
+                    convex_hull.Add(up[i]);
+            for (int i = down.Count - 1; i >= 0; --i)
+                if (!convex_hull.Contains(down[i]))
+                    convex_hull.Add(down[i]);
+        }
+
         private void construct_Click(object sender, EventArgs e)
         {
-            
+            if (points.Count < 3) return;
+            Andrew();
+            Graphics g = Graphics.FromImage(pictureBox.Image);
+            for (int i = 0; i < convex_hull.Count - 1; ++i)
+                g.DrawLine(pen, convex_hull[i], convex_hull[i + 1]);
+            g.DrawLine(pen, convex_hull[0], convex_hull[convex_hull.Count - 1]);
+            pictureBox.Invalidate();
+            convex_hull.Clear();
         }
 
         private void clear_Click(object sender, EventArgs e)
         {
+            if (points.Count > 0)
+                points.Clear();
+            if(convex_hull.Count > 0)
+                convex_hull.Clear();
             Graphics g = Graphics.FromImage(pictureBox.Image);
             g.Clear(Color.White);
             g.Dispose();
@@ -45,13 +96,18 @@ namespace Andrew
             if (pictureBox.Image != null)
                 pictureBox.Image.Dispose();
             pictureBox.Image = bmp;
+            points = new List<Point>();
+            convex_hull = new List<Point>();
         }
 
         private void pictureBox_MouseClick(object sender, MouseEventArgs e)
         {
             Graphics g = Graphics.FromImage(pictureBox.Image);
-            g.DrawEllipse(pen, new Rectangle(e.X, e.Y, 1, 1));
+            g.DrawRectangle(pen, new Rectangle(e.X, e.Y, 1, 1));
             g.Dispose();
+            //Bitmap bmp = pictureBox.Image as Bitmap;
+            //bmp.SetPixel(e.X, e.Y, Color.Black);
+            points.Add(new Point(e.X, e.Y));
             pictureBox.Invalidate();
         }
     }
